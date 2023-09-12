be8a323279ba4b7c091b89f07ac98768e5982334
Rodrigo Erthal <rrerthal@gmail.com>
2023-09-12 11:53:52 +0200

    initial popcorn time settings

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+
+
+function Main(){
+    return(
+        <h1>This is the Main</h1>
+    )
+}
+export default Main;
